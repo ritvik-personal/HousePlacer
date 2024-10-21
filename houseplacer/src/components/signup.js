fba19601b05143b6e869e38ce4db8563fcffb79d
Aneesh Gadgil <aneesh.gadgil04@gmail.com>
2024-10-21 17:59:16 -0400

    Fixed error

--- houseplacer/src/components/signup.js
+++ houseplacer/src/components/signup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Signup.css'
+import './signup.css'
 import EmailIcon from '@mui/icons-material/Email';
 import PersonIcon from '@mui/icons-material/Person';
 import HttpsIcon from '@mui/icons-material/Https';
