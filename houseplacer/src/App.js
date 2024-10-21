f1d296bc31373eb7852871a2e75e13dc3660ed50
Aneesh Gadgil <aneesh.gadgil04@gmail.com>
2024-10-21 17:03:14 -0400

    authen. page added in

--- houseplacer/src/App.js
+++ houseplacer/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import Signup from './components/Signup'; // This should match the file name casing exactly.
+
+
+
 
 
 function App() {
@@ -9,6 +13,7 @@ function App() {
        <p>Welcome to the CWRU centralized application for all housing needs! For first time users, please create an account. 
         For those returning users, feel free to navigate to the relevant sign in portal. </p>
       </header>
+      <Signup></Signup>
     </div>
   );
 }
--- houseplacer/src/components/signup.js
+++ houseplacer/src/components/signup.js
@@ -1,11 +1,40 @@
 import React from 'react'
-import './signup.css'
+import './Signup.css'
 import EmailIcon from '@mui/icons-material/Email';
+import PersonIcon from '@mui/icons-material/Person';
+import HttpsIcon from '@mui/icons-material/Https';
 
-const signup = () => {
+
+const Signup = () => {
+
+    //const[action, setAction] = React.useState("Create Account");
+
     return(
-        <div>
-
+        <div className="container">
+            <div className="header">
+            <div className="text">Sign up</div>
+            <div className="underline"></div>
+            </div>
+            <div className="inputs">
+            <div className="input">
+                <EmailIcon></EmailIcon>
+                <input type="email" placeholder='email'/>
+            </div>
+            <div className="input">
+                <PersonIcon></PersonIcon>
+                <input type="text" placeholder='username'/>
+            </div>
+            <div className="input">
+                <HttpsIcon></HttpsIcon>
+                <input type="password" placeholder='password'/>
+            </div>
+            </div>
+            <div className="submit-container">
+                <div className="submit">Create Account</div>
+                <div className="existingStudent">I am a student</div>
+                <div className='existingManager'>I am a manager</div>
+            </div>
         </div>
     )
 }
+export default Signup;
