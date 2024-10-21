6138d9793f4e88fa9b2f1e865c78b20597939cc2
Aneesh Gadgil <aneesh.gadgil04@gmail.com>
2024-10-21 15:10:29 -0400

    Update

---
+++ houseplacer/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       HousePlacer
+       <p>Welcome to the CWRU centralized application for all housing needs! For first time users, please create an account. 
+        For those returning users, feel free to navigate to the relevant sign in portal. </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ houseplacer/src/components/signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './signup.css'
+import EmailIcon from '@mui/icons-material/Email';
+
+const signup = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
