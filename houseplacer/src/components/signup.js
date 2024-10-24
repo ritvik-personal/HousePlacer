796df6dc84f8f91f3b189cd8e8614b6a48e9d6f5
Aneesh Gadgil <aneesh.gadgil04@gmail.com>
2024-10-24 18:56:00 -0400

    some more

--- houseplacer/src/components/signup.js
+++ houseplacer/src/components/signup.js
@@ -11,6 +11,8 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import { toast, ToastContainer } from 'react-toastify';
 
 
 const Signup = () => {
@@ -21,15 +23,62 @@ const Signup = () => {
     const[email, setEmail] = React.useState('');
     const[username, setUsername] = React.useState('');
     const[password, setPassword] = React.useState('');
-    const[classification, setClassification] = React.useState('');
+    const[classification, setClassification] = React.useState('Student');
     const[registerStatus, setRegisterStatus] = React.useState('');
+    const[errorStatements, setErrorStatements] = React.useState(['', '', '', '']);
+    const[emailError, setEmailError] = React.useState(false);
+    const[nameError, setNameError] = React.useState(false);
+    const[classificationError, setClassificationError] = React.useState(false);
+    const[passwordError, setPasswordError] = React.useState(false);
 
     const handleSubmit = (e) =>{
         e.preventDefault();
     };
 
+    const validate = () => {
+        let valid = true;
+        const errors = ['', '', '', '']
+    
+        if(email === '' || !email.includes("@")){
+            setEmailError(true);
+            errors[0] = 'Email is invalid.';
+            valid = false;
+        } 
+    
+        if(username.length < 5){
+            setNameError(true);
+            errors[1] =  'Username is invalid. Remember your username must be at least 5 characters long.';
+            valid = false;
+        } 
+    
+        if(classification === ''){
+            setClassificationError(true);
+            errors[2] = 'You must identify as either a student or manager.';
+            valid = false;
+        } 
+    
+        if(password.length < 8){
+            setPasswordError(true);
+            errors[3] = 'Password is invalid. Remember, your password must be at least 8 characters long.';
+            valid = false;
+        } 
+        setErrorStatements(errors);
+        return valid;
+    };
+    
+
+    
+
     const register = (e) =>{
         e.preventDefault();
+        if(validate() == false){
+            toast.error("Please make sure all fields are completed correctly.",
+                {position: "bottom-right", 
+                    autoClose: 5000,
+                }
+            )
+        }
+        else{
         axios.post("http://localhost:8081/register",{
             classification: classification,
             email: email,
@@ -38,48 +87,62 @@ const Signup = () => {
         }).then((response) => {
             if(response.data.message){
                 setRegisterStatus(response.data.message);
+                if(classification !== "Student"){
+                    navigate("/managerdashboard");
+                }
+                else{
+                    navigate("/studentdashboard");
+                }
             }
             else{
                 setRegisterStatus("Account created successfully");
             }
         })
     }
+    }
 
-    return(
+    return (
         <div className="container">
-            <div className="header">
+          <div className="header">
             <div className="text">Sign up</div>
             <div className="underline"></div>
-            </div>
-            <div className="inputs">
-            
-            
+          </div>
+          <div className="inputs">
+      
             <FormLabel id="demo-row-radio-buttons-group-label" className="radio-label"> I am signing up as a:</FormLabel>
-            <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group"className="radio-group" onChange={(e) => setClassification(e.target.value)}>
-                <FormControlLabel value="Student" control={<Radio />} label="Student" />
-                <FormControlLabel value="Manager" control={<Radio />} label="Manager" />
+            <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" value={classification} className="radio-group" onChange={(e) => setClassification(e.target.value)}>
+              <FormControlLabel value="Student" control={<Radio />} label="Student" />
+              <FormControlLabel value="Manager" control={<Radio />} label="Manager" />
             </RadioGroup>
-           
-
-            <div className="input">
-                <EmailIcon></EmailIcon>
-                <input type="email" placeholder='Email'  onSubmit={(e) => handleSubmit(e)} onChange={(e) => setEmail(e.target.value)}/>
+      
+            <div className={`input ${emailError ? 'error' : ''}`}>
+              <EmailIcon />
+              <input type="email" placeholder='Email' onSubmit={(e) => handleSubmit(e)} onChange={(e) => setEmail(e.target.value)} />
+              {emailError && <span className="error-message">{errorStatements[0]}</span>}
             </div>
-            <div className="input">
-                <PersonIcon></PersonIcon>
-                <input type="text" placeholder='Username'  onSubmit={(e) => handleSubmit(e)} onChange={(e) => setUsername(e.target.value)}/>
+      
+            <div className={`input ${nameError ? 'error' : ''}`}>
+              <PersonIcon />
+              <input type="text" placeholder='Username' onSubmit={(e) => handleSubmit(e)} onChange={(e) => setUsername(e.target.value)} />
+              {nameError && <span className="error-message">{errorStatements[1]}</span>}
             </div>
-            <div className="input">
-                <HttpsIcon></HttpsIcon>
-                <input type="password" placeholder='Password'  onSubmit={(e) => handleSubmit(e)} onChange={(e) => setPassword(e.target.value)}/>
+      
+            <div className={`input ${passwordError ? 'error' : ''}`}>
+              <HttpsIcon />
+              <input type="password" placeholder='Password' onSubmit={(e) => handleSubmit(e)} onChange={(e) => setPassword(e.target.value)} />
+              {passwordError && <span className="error-message">{errorStatements[3]}</span>}
             </div>
-            </div>
-            <div className="submit-container">
+      
+          </div>
+      
+          <div className="submit-container">
             <div className="submit" onClick={(e) => register(e)}>Create Account</div>
             <div className="existingStudent" onClick={() => navigate("./studentportal")}>Already a student</div>
             <div className="existingManager" onClick={() => navigate("./managerportal")}>Already a manager</div>
-            </div>
+          </div>
         </div>
-    )
+      );
+      
+    
 }
 export default Signup;
