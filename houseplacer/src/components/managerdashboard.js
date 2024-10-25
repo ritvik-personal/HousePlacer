f30901d89407bb74e76508bfaffe03cdfd3de521
ritvik-personal <136015820+ritvik-personal@users.noreply.github.com>
2024-10-25 14:54:37 +0000

    edit to managerdashboard

--- houseplacer/src/components/managerdashboard.js
+++ houseplacer/src/components/managerdashboard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './studentdashboard.css';
+import { useNavigate } from 'react-router-dom';
 
 const ManagerDashboard = () => {
     const[activeTab, setActiveTab] = useState("Saved Properties");
